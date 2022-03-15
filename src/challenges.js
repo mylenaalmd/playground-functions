d594ee7e409aa46b12b26cb85a4c2f44c0b06002
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-03-15 20:41:21 -0300

    atualizando desafios

--- src/challenges.js
+++ src/challenges.js
@@ -36,15 +36,9 @@ function footballPoints(wins, ties) {
 console.log(footballPoints(0,0));
 
 // Desafio 6
-function highestCount(numbers) {
-  for (let i = 0; i < array.length; i++){
-    if (numbers[i] >= repeticao){
-     repeticao = numbers[i];
-    }
-  } repeticao = repeticao.length
-  return repeticao
+function highestCount() {
+  
 }
-console.log(highestCount[9,1,2,3,9,5,7]);
 
 // Desafio 7
 function catAndMouse() {
@@ -53,65 +47,29 @@ function catAndMouse() {
 
 // Desafio 8
 function fizzBuzz(numeros) {
-let armazeneString = [0];
+let armazeneString = [];
     for (let i = 0; i < numeros.length; i++){
       if (numeros[i] % 3 === 0  && numeros[i] % 5 === 0){
-        armazeneString.push('fizzBuzz');
+        armazeneString.push("fizzBuzz");
       } else if (numeros[i] % 3 ===0 && numeros[i] % 5 !== 0){
-        armazeneString.push('fizz');
+        armazeneString.push("fizz");
       } else if (numeros[i] % 3 !== 0 && numeros[i] % 5 === 0){
-        armazeneString.push('buzz');
+        armazeneString.push("buzz");
       } else {
-        armazeneString.push('bug!');
+        armazeneString.push("bug!");
       }
   }return armazeneString;
 }
 console.log(fizzBuzz([2,15,7,9,45]));
 
 // Desafio 9
-function encode(string) {
-  let vogal = [a,e,i,o,u];
-  let novaFrase = [];
-  for (let i=0; i < string.lenght; i++){
-    if (string[i] === vogal){
-      if (vogal === a){
-        novaFrase.push(1);
-      } else if (vogal === e){
-        novaFrase.push(2);
-      } else if (vogal === i){
-        novaFrase.push(3);
-      } else if (vogal === o){
-        novaFrase.push(4);
-      } else if (vogal === u){
-        novaFrase.push(5);
-      }
-    } else {
-      novaFrase.push(string[i]);
-    }
-  } return novaFrase;
-} console.log(encode("hi there!"));
+function encode() {
 
-function decode(novaFrase) {
-    let number = [1,2,3,4,5];
-    let frase = [];
-    for (let i=0; i < novaFrase.lenght; i++){
-      if (novaFrase[i] === number){
-        if (number === 1){
-          frase.push(a);
-        } else if (number === 2){
-          frase.push(e);
-        } else if (number === 3){
-          frase.push(i);
-        } else if (number === 4){
-          frase.push(o);
-        } else if (number === 5){
-          frase.push(u);
-        }
-      } else {
-        novaFrase.push(frase[i]);
-      }
-    } return frase;
-  } console.log(encode(frase));
+}
+  
+function decode() {
+    
+}
 
 // Desafio 10
 function techList() {
